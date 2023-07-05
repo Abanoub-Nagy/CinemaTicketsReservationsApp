479d1d8e6d79f326664068a7ec55281e94c8ba5d
Abanoub <abanoubnagy24@gmail.com>
2023-07-05 14:51:20 +0300

    add theme used in project

--- app/src/main/java/com/example/cinematicketsreservationsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/cinematicketsreservationsapp/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange80,
+    secondary = DarkGrayBrown,
+    tertiary = OrangeGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange40,
+    secondary = DarkGrayBrown40,
+    tertiary = OrangeGrey40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
