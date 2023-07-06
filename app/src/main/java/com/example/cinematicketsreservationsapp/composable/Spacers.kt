7971f4dea53f453eb7476ab4e40e283d0b31f03c
Abanoub <abanoubnagy24@gmail.com>
2023-07-06 07:38:36 +0300

    create file spacer include all spacers

---
+++ app/src/main/java/com/example/cinematicketsreservationsapp/composable/Spacers.kt
@@ -0,0 +1,56 @@
+package com.example.cinematicketsreservationsapp.composable
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerHorizontal4() {
+    Spacer(modifier = Modifier.width(4.dp))
+}
+
+@Composable
+fun SpacerHorizontal8() {
+    Spacer(modifier = Modifier.width(8.dp))
+}
+
+@Composable
+fun SpacerHorizontal16() {
+    Spacer(modifier = Modifier.width(16.dp))
+}
+
+
+
+
+@Composable
+fun SpacerVertical4() {
+    Spacer(modifier = Modifier.height(4.dp))
+}
+
+@Composable
+fun SpacerVertical14() {
+    Spacer(modifier = Modifier.height(14.dp))
+}
+
+@Composable
+fun SpacerVertical8() {
+    Spacer(modifier = Modifier.height(8.dp))
+}
+
+@Composable
+fun SpacerVertical16() {
+    Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Composable
+fun SpacerVertical24() {
+    Spacer(modifier = Modifier.height(24.dp))
+}
+
+@Composable
+fun SpacerVertical32() {
+    Spacer(modifier = Modifier.height(32.dp))
+}
